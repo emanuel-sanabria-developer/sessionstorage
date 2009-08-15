50a90ff826e96458d27d9032b9092f8ed534663f
andrea.giammarchi <andrea.giammarchi@36bee900-878c-11de-9e21-49812d2d8162>
2009-08-15 01:00:35 +0000

    Version 1.1

--- build/sessionstorage.js
+++ build/sessionstorage.js
@@ -1,3 +1,222 @@
+/** HTML5 sessionStorage
+ * @build       2009-08-15 00:55:24
+ * @author      Andrea Giammarchi
+ * @license     Mit Style License
+ * @project     http://code.google.com/p/sessionstorage/
+ */
+
+if(typeof sessionStorage === "undefined")(function(window){
+
+/** RC4 Stream Cipher
+ *  http://www.wisdom.weizmann.ac.il/~itsik/RC4/rc4.html
+ * -----------------------------------------------
+ * @description     A quick stream cipher object able to encode
+ *                  or decode whatever string using
+ *                  a random key from lennght 1 to 256
+ *
+ * @author          this JavaScript porting by Andrea Giammarchi
+ * @license         Mit Style License
+ * @blog            http://webreflection.blogspot.com/
+ * @version         1.0
+ * @compatibility   hopefully every browser
+ */
+ var RC4 = (function(fromCharCode, random){
+    return {
+
+        /** RC4.decode(key:String, data:String):String
+         * @description     given a data string encoded with the same key
+         *                  generates original data string.
+         * @param   String  key precedently used to encode data
+         * @param   String  data encoded using same key
+         * @return  String  decoded data
+         */
+        decode:function(key, data){
+            return this.encode(key, data);
+        },
+
+        /** RC4.encode(key:String, data:String):String
+         * @description     encode a data string using provided key
+         * @param   String  key to use for this encoding
+         * @param   String  data to encode
+         * @return  String  encoded data. Will require same key to be decoded
+         */
+        encode:function(key, data){
+            // cannot spot anything redundant that
+            // could make this algo faster!!! Good Stuff RC4!
+            for(var
+                length = key.length, len = data.length,
+                decode = [], a = [],
+                i = 0, j = 0, k = 0, l = 0, $;
+                i < 256; ++i
+            )   a[i] = i;
+            for(i = 0; i < 256; ++i){
+                j = (j + ($ = a[i]) + key.charCodeAt(i % length)) % 256;
+                a[i] = a[j];
+                a[j] = $;
+            };
+            for(j = 0; k < len; ++k){
+                i = k % 256;
+                j = (j + ($ = a[i])) % 256;
+                length = a[i] = a[j];
+                a[j] = $;
+                decode[l++] = fromCharCode(data.charCodeAt(k) ^ a[(length + $) % 256]);
+            };
+            return decode.join("");
+        },
+
+        /** RC4.key(length:Number):String
+         * @description     generate a random key with arbitrary length
+         * @param   Number  the length of the generated key
+         * @return  String  a randomly generated key
+         */
+        key:function(length){
+            for(var i = 0, key = []; i < length; ++i)
+                key[i] = fromCharCode((random() * 256) << 0);
+            return key.join("");
+        }
+    }
+    // I like to freeze stuff in interpretation time
+    // it makes things a bit safer when obtrusive libraries
+    // are around
+})(window.String.fromCharCode, window.Math.random);
+
+/** Linear String Storage
+ * -----------------------------------------------
+ * @description     A Linear String Storage is a way to save
+ *                  unique keys with related values inside a string.
+ *
+ * @author          Andrea Giammarchi
+ * @license         Mit Style License
+ * @blog            http://webreflection.blogspot.com/
+ * @version         1.0
+ * @compatibility   Internet Explorer, Chrome, Opera (unobtrusive for others)
+ * @protocol        Linear String Storage Protocol Specs
+ * -----------------------------------------------
+ * c       = special separator char
+ * key     = key to use as value reference
+ * length  = value length
+ * value   = value to store
+ * entry   = c + key + c + length + c + value
+ * -----------------------------------------------
+ * c key c length c value
+ * o[   ]o[      ]o[     ]
+ *
+ * key must be a string
+ * value must be a string
+ * both key and value should be converted
+ * both key and value cannot contain the special "c" char (replacement)
+ * entry can always be appended into the linear string storage
+ * entry cannot exist, or there could be more than an entry
+ * if a key was already present, its entry has to be removed and the new one appended
+ * -----------------------------------------------
+ */
+var LSS = (function(window){
+
+    /** new LSS([storage:Object, key:String, data:String])
+     * @param   Object      an optional object to use as storage or directly a linear String.
+     * @param   String      the Object property with the linear string to manage.
+     * @param   String      optional data to preserve as initial string (when clear is performed, data is preserved)
+     * @example
+     *          // persistent storage via window.name property
+     *          var s = new LSS(window, "name");
+     *
+     *          // temporary storage via existent string
+     *          var s = new LSS(window.name);
+     *
+     *          // temporary storage with empty string
+     *          var s = new LSS();
+     */
+    function LSS(_storage, _key, _data){
+        this._data = _data || "";
+        if(this._key = _key)
+            this._storage = _storage;
+        else {
+            this._storage = {_key:_storage || ""};
+            this._key = "_key";
+        };
+    };
+
+    /** @description    special character to use as separator
+     */
+    LSS.prototype.c = String.fromCharCode(1);
+
+    /** this.clear(void):void
+     * @description     reset the storage string
+     */
+    LSS.prototype.clear = function(){
+        this._storage[this._key] = this._data;
+    };
+
+    /** this.del(key:String):void
+     * @description     remove a key if present and its related value.
+     * @param   String  the key to remove
+     */
+    LSS.prototype.del = function(key){
+        var data = this.get(key);
+        if(data !== null)
+            this._storage[this._key] = this._storage[this._key].replace(escape.call(this, key, data), "");
+    };
+
+    /** this.escape(data:String):String
+     * @description     escape a generic string. By default it uses window.escape function.
+     *                  Please note if modified it should preserve the used special character.
+     * @param   String  data to escape
+     * @return  String  escaped data that can NOT contain the special character.
+     */
+    LSS.prototype.escape   = window.escape;
+
+    /** this.get(key:String):String
+     * @description     retrieve stored data if key is present.
+     * @param   String  key related to stored data
+     * @return  String  stored data or null if key was not present.
+     */
+    LSS.prototype.get = function(key){
+        var _storage = this._storage[this._key],
+            c = this.c,
+            i = _storage.indexOf(key = c + this.escape(key) + c),
+            data = null
+        ;
+        if(-1 < i){
+            i = _storage.indexOf(c, i + key.length - 1) + 1;
+            data = _storage.substring(i, i = _storage.indexOf(c, i));
+            data = this.unescape(_storage.substr(++i, data));
+        };
+        return data;
+    };
+
+    /** this.set(key:String, data:String):void
+     * @description     stores data relating it with the specified key.
+     *                  pleae note if the key was present, it is removed
+     *                  and the new key/value pair appended.
+     * @param   String  key to related with specified data.
+     * @return  String  data to store.
+     */
+    LSS.prototype.set = function(key, data){
+        this.del(key);
+        this._storage[this._key] += escape.call(this, key, data);
+    };
+
+    /** this.unescape(data:String):String
+     * @description     unescape a generic string. This should compatible with escape.
+     * @param   String  data to unescape
+     * @return  String  unescaped data that could contain the special char as well.
+     */
+    LSS.prototype.unescape = window.unescape;
+
+    /** escape(key:String, data:String):String
+     * @description     private scope callback. A shortcut to retrieve an entry via key and data
+     */
+    function escape(key, data){
+        var c = this.c;
+        key = this.escape(key);
+        data = this.escape(data);
+        return c.concat(key, c, data.length, c, data);
+    };
+
+    return LSS;
+
+})(window);
+
 /** HTML5 sessionStorage cross-browser implementation
  * @description     sessionStorage object is an HTML5 working draft
  *                  which aim is to remove cookie limits.
@@ -244,3 +463,5 @@ if(!cache.indexOf) cache.indexOf = function(data){
 
 // it's about the time to create the sessionStorage object, isn't it?
 window.sessionStorage = new sessionStorage;
+
+})(window);
