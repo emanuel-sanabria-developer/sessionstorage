d2500f67aba72e3f4ebdd389b7b27103e856bfff
andrea.giammarchi <andrea.giammarchi@36bee900-878c-11de-9e21-49812d2d8162>
2009-08-15 18:20:01 +0000

    new test for events ... if only there is something to test ...

--- trunk/build/sessionstorage.js
+++ trunk/build/sessionstorage.js
@@ -1,5 +1,5 @@
 /** HTML5 sessionStorage
- * @build       2009-08-15 17:41:35
+ * @build       2009-08-15 18:19:24
  * @author      Andrea Giammarchi
  * @license     Mit Style License
  * @project     http://code.google.com/p/sessionstorage/
@@ -10,8 +10,14 @@ if(typeof sessionStorage === "undefined")(function(window){
 
 // this script could have been included via iframe so we would like to use the top context as storage area
 // but if the iframe is not part of the domain below check could generate an error
-var top = window;
-try{while(top !== top.top)top = top.top;}catch(e){};
+var // the top pointer is used in any case to point to the top context level
+    top = window
+;
+try {
+    while(top !== top.top)
+        top = top.top;
+} catch(e) {
+};
 
 /** RC4 Stream Cipher
  *  http://www.wisdom.weizmann.ac.il/~itsik/RC4/rc4.html
